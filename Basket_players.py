3b3e26512a7a1a35d2c8b010e3ba36d69e8f835f
JRAdev11 <jreyarencibia@gmail.com>
2024-11-19 09:33:44 +0000

    first commit

---
+++ Basket_players.py
@@ -0,0 +1,7 @@
+import functions_players
+
+heights = [188, 198, 199, 150, 196, 2000, 197, 215, 210]
+# We call the function
+data = functions_players.players(heights)
+# We print the function on the console
+print(data)
---
+++ functions_players.py
@@ -0,0 +1,20 @@
+def players(heights):
+    # Definition of the height ranges by position
+    positions = {
+        "C":  (208, 230),  # Center         (Pívot)
+        "PF": (203, 210),  # Power Forward  (Ala-Pívot)
+        "SF": (195, 205),  # Small Forward  (Alero)
+        "SG": (193, 198),  # Shooting Guard (Escolta)
+        "PG": (180, 193),  # Point Guard    (Base)
+    }
+    
+    # We define the counters for each position
+    position_counts = {"PG": 0, "SG": 0, "SF": 0, "PF": 0, "C": 0}
+    
+    # We review each height and assign it to the appropriate range
+    for height in heights:
+        for pos, (low, high) in positions.items():
+            if low <= height <= high:
+                if position_counts[pos] < height:
+                    position_counts[pos] = height
+    return position_counts
