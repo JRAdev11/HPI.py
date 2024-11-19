0ca9879849f3759db3784a97196de05b90db178b
JRAdev11 <jreyarencibia@gmail.com>
2024-11-19 21:46:23 +0000

    Update tallest_basket_players.py

--- tallest_basket_players.py
+++ tallest_basket_players.py
@@ -1,3 +1,6 @@
+# This code is licensed under the MIT License.
+# See the LICENSE file in the root of this repository for details.
+
 import functions_players
 
 heights = [188, 198, 199, 150, 196, 2000, 197, 215, 210]
